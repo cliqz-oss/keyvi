9ee15e8808031f5594a761a3a9d8dd5b7c60186e
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-07-06 15:08:59 +0200

    add a test for setnx

--- keyvi-server/tests/basic_test.py
+++ keyvi-server/tests/basic_test.py
@@ -2,7 +2,7 @@
 # Usage: py.test tests
 
 from simple_test_server import tmp_server
-import time
+import pykeyvi
 
 def test_set():
     with tmp_server() as t:
@@ -12,3 +12,18 @@ def test_set():
         t.sync()
         x = reader.call('get', 'a')
         assert x is not None
+        assert {"b": 3} == pykeyvi.Match.loads(x).GetValue()
+
+def test_setnx():
+    with tmp_server() as t:
+        writer = t.get_writer()
+        writer.call('set', 'a', '{"b":3}')
+        writer.call('commit', False)
+        writer.call('setnx', 'a', '{"c":6}')
+        writer.call('setnx', 'a', '{"d":2}')
+
+        reader = t.get_reader()
+        t.sync()
+        x = reader.call('get', 'a')
+        assert x is not None
+        assert {"b": 3} == pykeyvi.Match.loads(x).GetValue()

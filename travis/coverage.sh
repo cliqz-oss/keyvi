f6f51c14a58ef80b560e19a0e4605f54dcdbc9ee
Hendrik Muhs <hendrik@cliqz.com>
2016-09-28 10:51:45 +0200

    test different symlink

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -2,8 +2,8 @@
 set -ev
 
 # workaround for coverage measurement: symlink cpp source:
-cd pykeyvi/src
-ln -s ../../keyvi/src/cpp/ .
+cd pykeyvi
+ln -s ../keyvi/ .
 cd ../..
 
 echo "$PWD"
@@ -12,7 +12,7 @@ export TEST="$PWD"
 coveralls   -r keyvi -b keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
             -e 3rdparty -E '.*/tests/*.cpp' \
             --dump keyvi.cov_report > /dev/null
-coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp \-s '"$PWD"'/keyvi' \
+coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
             -E '.*3rdparty' -E '.*/pykeyvi.cpp' -E '.*autowrap.*' \
             --dump pykeyvi.cov_report --follow-symlinks > /dev/null
 

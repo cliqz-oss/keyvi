0116ba13f365d39e63bcfb2f1b9a76132e76e32c
Hendrik Muhs <hendrik@cliqz.com>
2016-09-28 17:24:18 +0200

    another try

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -12,7 +12,7 @@ export TEST="$PWD"
 coveralls   -r keyvi -b keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
             -e 3rdparty -E '.*/tests/*.cpp' \
             --dump keyvi.cov_report > /dev/null
-coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
+coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp \-s '"$PWD"'/pykeyvi/keyvi' \
             -E '.*3rdparty' -E '.*/pykeyvi.cpp' -E '.*autowrap.*' \
             --dump pykeyvi.cov_report --follow-symlinks > /dev/null
 

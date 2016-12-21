e5c3e13c29085d85a6f44b3dabc2ea735e8da30d
Hendrik Muhs <hendrik@cliqz.com>
2016-12-21 08:17:33 +0100

    manylinux different versions

--- travis/build_manylinux_wheels.sh
+++ travis/build_manylinux_wheels.sh
@@ -3,6 +3,23 @@ set -ev
 
 cd /keyvi/pykeyvi/
 
+case "${PYTHON_VERSION}" in
+  27)
+      PYBIN=/opt/python/cp27-cp27mu/bin
+      ;;
+  33)
+      PYBIN=/opt/python/cp33-cp33m/bin
+      ;;
+  34)
+      PYBIN=/opt/python/cp34-cp34m/bin
+      ;;
+  35)
+      PYBIN=/opt/python/cp35-cp35m/bin
+      ;;
+  *)
+      echo "PYTHON_VERSION not set"
+esac
+
 PYBIN=/opt/python/cp27-cp27mu/bin
 
 ${PYBIN}/python setup.py bdist_wheel

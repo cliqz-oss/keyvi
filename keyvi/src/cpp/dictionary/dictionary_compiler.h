062d60167448c899c261d98c635213248911ffc2
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-01-22 20:06:12 +0100

    try string instead of native

--- keyvi/src/cpp/dictionary/dictionary_compiler.h
+++ keyvi/src/cpp/dictionary/dictionary_compiler.h
@@ -118,7 +118,7 @@ class DictionaryCompiler final {
 
     params_[TEMPORARY_PATH_KEY] =
         util::mapGet(params_, TEMPORARY_PATH_KEY,
-                     boost::filesystem::temp_directory_path().native());
+                     boost::filesystem::temp_directory_path().string());
 
     TRACE("tmp path set to %s", params_[TEMPORARY_PATH_KEY].c_str());
 

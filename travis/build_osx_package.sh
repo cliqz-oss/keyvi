78cc912eb0d39845f4320c41cffe9842504ce151
narekgharibyan <narekgharibyan@gmail.com>
2017-02-09 17:50:21 +0100

    - updated CMake compile flags  - added coverage CMAKE_BUILD_TYPE  - using cmake for travis builds

--- travis/build_osx_package.sh
+++ travis/build_osx_package.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -ev
 
-cd keyvi
-scons -j 4 mode=release
-cd ..
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=release ..
+make -j 4
 
 # use python from pyenv
 PYENV_ROOT="$HOME/.pyenv"

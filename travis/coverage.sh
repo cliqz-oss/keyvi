a674c437c2fee52b27b70a67261e1cc041199d05
narekgharibyan <narekgharibyan@gmail.com>
2017-02-14 17:43:53 +0100

    - next try

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -ev
 
+
+coveralls   -r . -b build/ -i keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
+            -e build/keyvi/3rdparty -e keyvi/3rdparty  \
+            --dump keyvi.cov_report > /dev/null
+
 # workaround for coverage measurement: symlink cpp source:
 cd pykeyvi/src
 ln -s ../../keyvi/src/cpp/ .
 cd ../..
 
-coveralls   -r . -b build/ -i keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
-            -e build/keyvi/3rdparty -e keyvi/3rdparty  \
-            --dump keyvi.cov_report > /dev/null
 coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp \-s '"$PWD"'/pykeyvi/keyvi' \
             -E '.*3rdparty' -E '.*/pykeyvi.cpp' -E '.*autowrap.*' \
             --dump pykeyvi.cov_report --follow-symlinks > /dev/null

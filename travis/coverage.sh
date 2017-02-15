eacd8a5e5ab314eec1e78161eaec1001507215da
narekgharibyan <narekgharibyan@gmail.com>
2017-02-15 11:08:43 +0100

    - test

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -7,14 +7,14 @@ coveralls   -r . -b build/ -i keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-l
             --dump keyvi.cov_report > /dev/null
 
 # workaround for coverage measurement: symlink cpp source:
-cd pykeyvi/src
-ln -s ../../keyvi/src/cpp/ .
-cd ../..
-
-coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp \-s '"$PWD"'/pykeyvi/keyvi' \
-            -E '.*3rdparty' -E '.*/pykeyvi.cpp' -E '.*autowrap.*' \
-            --dump pykeyvi.cov_report --follow-symlinks > /dev/null
+#cd pykeyvi/src
+#ln -s ../../keyvi/src/cpp/ .
+#cd ../..
+#
+#coveralls   -r pykeyvi -b pykeyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp \-s '"$PWD"'/pykeyvi/keyvi' \
+#            -E '.*3rdparty' -E '.*/pykeyvi.cpp' -E '.*autowrap.*' \
+#            --dump pykeyvi.cov_report --follow-symlinks > /dev/null
 
 export COVERALLS_REPO_TOKEN=${COVERALLS_REPO_TOKEN}
 
-coveralls-merge  keyvi.cov_report pykeyvi.cov_report
+coveralls-merge  keyvi.cov_report

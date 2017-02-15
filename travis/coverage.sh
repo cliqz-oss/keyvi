f41ba39b1a6e93512e553a64a5a7e126feba955e
narekgharibyan <narekgharibyan@gmail.com>
2017-02-15 11:58:36 +0100

    - excluding tests

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -3,7 +3,8 @@ set -ev
 
 
 coveralls   -r . -b build/ -i keyvi --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
-            -e build/keyvi/3rdparty -e keyvi/3rdparty -e keyvi/tests -e pykeyvi \
+            -e build/keyvi/3rdparty -e keyvi/3rdparty -e pykeyvi \
+            -E '.*/tests/*.cpp' \
             --dump keyvi.cov_report > /dev/null
 
 # workaround for coverage measurement: symlink cpp source:

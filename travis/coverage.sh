0419abdad8f1b47e3424072c805a390a2660b6ba
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-02-22 09:31:12 +0100

    remove sed workaround

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-coveralls   -r . -b build/ -i keyvi \
+coveralls   -r . -b keyvi/build/ -i keyvi \
             --gcov /usr/bin/gcov-4.8 --gcov-options '\-lp' \
             -e build/keyvi/3rdparty -e keyvi/3rdparty -e pykeyvi \
             -E '.*/tests/*.cpp' \
@@ -22,9 +22,6 @@ coveralls   -r . -b pykeyvi/ -i pykeyvi \
             -E '.*/src/extra/attributes_converter.h' \
             -E '.*/pykeyvi.cpp' \
             --dump pykeyvi.cov_report_tmp > /dev/null
-
-# workaround: remove 'pykeyvi' from source path before merge
-sed s/"pykeyvi\/keyvi"/"keyvi"/g pykeyvi.cov_report_tmp > pykeyvi.cov_report
 
 export COVERALLS_REPO_TOKEN=${COVERALLS_REPO_TOKEN}
 

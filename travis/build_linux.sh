8e8534cbe2592f53924d0e4afd617987e62b9db1
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-02-22 07:24:26 +0100

    adjust builld scripts after changing CMakelist.txt location

--- travis/build_linux.sh
+++ travis/build_linux.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -ev
 
+cd keyvi
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=$CONF ..
 make -j 4
 ./units_test_all
+cd ..
 
 cd ../pykeyvi
 python setup.py build --mode $CONF
--- travis/build_osx_package.sh
+++ travis/build_osx_package.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -ev
 
+cd keyvi
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=release ..
 make -j 4
+cd ..
 cd ..
 
 # use python from pyenv
